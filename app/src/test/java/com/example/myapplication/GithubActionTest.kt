e88dd8fd58d17834931dabc074e46791309d08be
idstudent <sm1374@everyfit.co.kr>
2024-02-28 16:27:51 +0900

    테스트코드 푸시

---
+++ app/src/test/java/com/example/myapplication/GithubActionTest.kt
@@ -0,0 +1,15 @@
+package com.example.myapplication
+
+import org.junit.Test
+
+class GithubActionTest {
+    @Test
+    fun testSuccess() {
+        assert(true)
+    }
+
+    @Test
+    fun testFail() {
+        assert(false)
+    }
+}
